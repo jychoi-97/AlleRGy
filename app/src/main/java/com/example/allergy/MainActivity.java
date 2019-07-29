a2a0f328a5ff00ed884716e7ba0b51dc38228d70
JeongIn <jngin002@daum.net>
2019-07-29 16:06:47 +0900

    기본 지도 띄우기 ..

--- app/src/main/java/com/example/allergy/MainActivity.java
+++ app/src/main/java/com/example/allergy/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.allergy;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null){
+
+            mapFragment=mapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+    }
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
     }
 }
